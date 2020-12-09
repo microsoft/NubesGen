82fb1592d059665cda3cc437958cc90a0357a4e8
Julien Dubois <julien.dubois@gmail.com>
2020-12-09 17:43:21 +0100

    generate simple REST server

---
+++ src/main/java/io/github/nubesgen/NubesgenApplication.java
@@ -0,0 +1,13 @@
+package io.github.nubesgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubesgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NubesgenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nubesgen/NubesgenApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nubesgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubesgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
