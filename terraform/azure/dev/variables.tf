8e8e70db155a73e70711eca6e9315d7a4c8ee6d2
Julien Dubois <julien.dubois@gmail.com>
2020-12-14 21:52:53 +0100

    Terraform code for deploying the app

---
+++ terraform/azure/dev/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group" {
+  description = "The resource group"
+  default = "nubesgen"
+}
+
+variable "application_name" {
+  description = "The name of your JHipster application"
+  default     = "sampleNubesApplication"
+}
+
+variable "location" {
+  description = "The Azure location where all resources in this example should be created"
+  default     = "westeurope"
+}
