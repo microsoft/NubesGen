37f59fc0d5514efea95347d9414ccf8d4bf303ab
Julien Dubois <julien.dubois@gmail.com>
2020-12-14 22:08:07 +0100

    Configure Terraform

--- terraform/azure/dev/main.tf
+++ terraform/azure/dev/main.tf
@@ -16,51 +16,6 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 
-resource "random_password" "password" {
-  length = 32
-  special = true
-  override_special = "_%@"
-}
-
-# This creates a MySQL server
-resource "azurerm_mysql_server" "main" {
-  name                = "${azurerm_resource_group.main.name}-mysql-server"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  administrator_login          = "jhipster"
-  administrator_login_password = random_password.password.result
-
-  sku_name   = "B_Gen5_1"
-  storage_mb = 5120
-  version    = "5.7"
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
-}
-
-# This is the database that our application will use
-resource "azurerm_mysql_database" "main" {
-  name                = "${azurerm_resource_group.main.name}_mysql_db"
-  resource_group_name = azurerm_resource_group.main.name
-  server_name         = azurerm_mysql_server.main.name
-  charset             = "utf8"
-  collation           = "utf8_unicode_ci"
-}
-
-# This rule is to enable the 'Allow access to Azure services' checkbox
-resource "azurerm_mysql_firewall_rule" "main" {
-  name                = "${azurerm_resource_group.main.name}-mysql-firewall"
-  resource_group_name = azurerm_resource_group.main.name
-  server_name         = azurerm_mysql_server.main.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
-}
-
 # This creates the plan that the service use
 resource "azurerm_app_service_plan" "main" {
   name                = "${var.application_name}-plan"
@@ -70,8 +25,8 @@ resource "azurerm_app_service_plan" "main" {
   reserved            = true
 
   sku {
-    tier = "PremiumV2"
-    size = "P3v2"
+    tier = "Basic"
+    size = "B1"
   }
 }
 
@@ -93,8 +48,5 @@ resource "azurerm_app_service" "main" {
 
     # These are app specific environment variables
     "SPRING_PROFILES_ACTIVE"     = "prod,azure"
-    "SPRING_DATASOURCE_URL"      = "jdbc:mysql://${azurerm_mysql_server.main.fqdn}:3306/${azurerm_mysql_database.main.name}?useUnicode=true&characterEncoding=utf8&useSSL=true&useLegacyDatetimeCode=false&serverTimezone=UTC"
-    "SPRING_DATASOURCE_USERNAME" = "${azurerm_mysql_server.main.administrator_login}@${azurerm_mysql_server.main.name}"
-    "SPRING_DATASOURCE_PASSWORD" = azurerm_mysql_server.main.administrator_login_password
   }
 }
