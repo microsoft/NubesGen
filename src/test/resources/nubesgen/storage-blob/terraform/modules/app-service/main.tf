a4923b6aaaa749107b9dfc7bec41d2c8feae0a95
Julien Dubois <julien.dubois@gmail.com>
2021-01-11 21:43:56 +0100

    Add tests for blob storage support

---
+++ src/test/resources/nubesgen/storage-blob/terraform/modules/app-service/main.tf
@@ -0,0 +1,39 @@
+
+# This creates the plan that the service use
+resource "azurerm_app_service_plan" "compute" {
+  name                = "plan-${var.application_name}-001"
+  resource_group_name = var.resource_group
+  location            = var.location
+
+  kind     = "Linux"
+  reserved = true
+
+  sku {
+    tier = var.sku_tier
+    size = var.sku_size
+  }
+}
+
+# This creates the service definition
+resource "azurerm_app_service" "compute" {
+  name                = "app-${var.application_name}-001"
+  resource_group_name = var.resource_group
+  location            = var.location
+  app_service_plan_id = azurerm_app_service_plan.compute.id
+  https_only          = true
+
+  site_config {
+    always_on        = true
+    linux_fx_version = "JAVA|11-java11"
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+
+    # These are app specific environment variables
+    "SPRING_PROFILES_ACTIVE"      = "prod,azure"
+    "AZURE_STORAGE_ACCOUNT_NAME"  = var.azure_storage_account_name
+    "AZURE_STORAGE_ACCOUNT_KEY"   = var.azure_storage_account_key
+    "AZURE_STORAGE_BLOB_ENDPOINT" = var.azure_storage_blob_endpoint
+  }
+}
