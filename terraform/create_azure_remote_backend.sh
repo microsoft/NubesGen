e3478a2711976fe802284bc9972f3a37dbfb2123
Julien Dubois <julien.dubois@gmail.com>
2021-02-03 15:13:52 +0100

    create script to configure Azure remote state

---
+++ terraform/create_azure_remote_backend.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+RESOURCE_GROUP_NAME=rg-terraform-001
+LOCATION=westeurope
+STORAGE_ACCOUNT_NAME=stterraform$RANDOM$RANDOM
+CONTAINER_NAME=tfstate
+# Create resource group
+az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
+# Create storage account
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+# Get storage account key
+ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query [0].value -o tsv)
+# Create blob container
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
+echo "resource_group_name: $RESOURCE_GROUP_NAME"
+echo "storage_account_name: $STORAGE_ACCOUNT_NAME"
+echo "container_name: $CONTAINER_NAME"
