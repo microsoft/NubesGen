691a7b757aa968996f0aecfa675915b411553f8d
Julien Dubois <julien.dubois@gmail.com>
2021-02-03 20:56:51 +0100

    create service principal

--- terraform/create_azure_remote_backend.sh
+++ terraform/create_azure_remote_backend.sh
@@ -11,6 +11,10 @@ az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query [0].value -o tsv)
 # Create blob container
 az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
+# Create service principal
+SUBSCRIPTION_ID=$(az account show --query id --output tsv)
+SERVICE_PRINCIPAL=$(az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$SUBSCRIPTION_ID" --name="http://sp-$RESOURCE_GROUP_NAME")
 echo "resource_group_name: $RESOURCE_GROUP_NAME"
 echo "storage_account_name: $STORAGE_ACCOUNT_NAME"
 echo "container_name: $CONTAINER_NAME"
+echo "service_principal: $SERVICE_PRINCIPAL"
