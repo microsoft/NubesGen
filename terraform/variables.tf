93cc63b484d77e849eed73e581d899b707071017
Julien Dubois <julien.dubois@gmail.com>
2021-02-03 15:14:13 +0100

    Use staging repo by default

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default     = "rg-nubesgen-001"
+  default     = "rg-nubesgen-staging-001"
 }
 
 variable "application_name" {
   description = "The name of your application"
-  default     = "nubesgen"
+  default     = "nubesgen-staging"
 }
 
 variable "location" {
