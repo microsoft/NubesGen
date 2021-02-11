b834adbf0741836795f4016d8066b5a899127557
Julien Dubois <julien.dubois@gmail.com>
2021-02-11 15:18:49 +0100

    fix Terraform variables to match prod

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default     = "rg-nubesgen-dev-001"
+  default     = "rg-nubesgen-001"
 }
 
 variable "application_name" {
   description = "The name of your application"
-  default     = "nubesgen-dev"
+  default     = "nubesgen"
 }
 
 variable "location" {
