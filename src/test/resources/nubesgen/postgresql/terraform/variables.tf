f193c2c90a436eaa5d39c77ae56aaa2c4b19ea0e
Julien Dubois <julien.dubois@gmail.com>
2021-03-02 22:51:42 +0100

    postgresql test case working with environments

--- src/test/resources/nubesgen/postgresql/terraform/variables.tf
+++ src/test/resources/nubesgen/postgresql/terraform/variables.tf
@@ -1,11 +1,11 @@
-variable "resource_group" {
-  description = "The resource group"
-  default     = "rg-nubesgen-testapp-postgresql-001"
-}
-
 variable "application_name" {
   description = "The name of your application"
   default     = "nubesgen-testapp-postgresql"
+}
+
+variable "environment" {
+  description = "The environment (dev, test, prod...)"
+  default     = ""
 }
 
 variable "location" {
