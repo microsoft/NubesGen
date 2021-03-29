57481fbd3273fff44f1fd08eccfb49bd3bf0a2fe
Kerim Satirli <k.satirli@gmail.com>
2021-03-29 14:27:59 +0200

    adds types and descriptions

--- terraform/modules/storage-blob/outputs.tf
+++ terraform/modules/storage-blob/outputs.tf
@@ -1,12 +1,15 @@
 output "azurerm_storage_account_name" {
-  value = azurerm_storage_account.storage-blob.name
+  description = "Storage Account Name"
+  value       = azurerm_storage_account.storage-blob.name
 }
 
 output "azurerm_storage_account_key" {
-  value     = azurerm_storage_account.storage-blob.primary_access_key
-  sensitive = true
+  description = "Storage Account Key (sensitive)"
+  value       = azurerm_storage_account.storage-blob.primary_access_key
+  sensitive   = true
 }
 
 output "azurerm_storage_blob_endpoint" {
-  value = azurerm_storage_account.storage-blob.primary_blob_endpoint
+  description = "Storage Blob Endpoint"
+  value       = azurerm_storage_account.storage-blob.primary_blob_endpoint
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,4 @@
 output "application_hostname" {
-  value = module.application.application_hostname
+  description = "Application Hostname"
+  value       = module.application.application_hostname
 }
