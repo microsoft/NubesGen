62b5f35845ee5efaab7d383886149f7f52ab57bf
Julien Dubois <julien.dubois@gmail.com>
2021-03-29 14:01:44 +0200

    Add descriptions to Terraform output variables

--- src/test/resources/nubesgen/cosmosdb-mongodb/terraform/modules/cosmosdb-mongodb/outputs.tf
+++ src/test/resources/nubesgen/cosmosdb-mongodb/terraform/modules/cosmosdb-mongodb/outputs.tf
@@ -1,8 +1,10 @@
 output "azure_cosmosdb_mongodb_database" {
-  value = azurerm_cosmosdb_mongo_database.cosmosdb.name
+  value       = azurerm_cosmosdb_mongo_database.cosmosdb.name
+  description = "The Cosmos DB database name."
 }
 
 output "azure_cosmosdb_mongodb_uri" {
-  value     = azurerm_cosmosdb_account.cosmosdb.connection_strings[0]
-  sensitive = true
+  value       = azurerm_cosmosdb_account.cosmosdb.connection_strings[0]
+  sensitive   = true
+  description = "The Cosmos DB connection string."
 }
