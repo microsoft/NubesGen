623264b06f55b600adb94d3a259789c4a606dd32
Julien Dubois <julien.dubois@gmail.com>
2021-11-24 13:01:51 +0100

    upgrade to the latest Terraform CAF provider

--- src/test/resources/nubesgen/terraform/asc-mysql-java/terraform/main.tf
+++ src/test/resources/nubesgen/terraform/asc-mysql-java/terraform/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
-      version = "1.2.6"
+      version = "1.2.9"
     }
   }
   backend "azurerm" {}
--- src/test/resources/nubesgen/terraform/redis/terraform/modules/redis/main.tf
+++ src/test/resources/nubesgen/terraform/redis/terraform/modules/redis/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurecaf = {
       source  = "aztfmod/azurecaf"
-      version = "1.2.6"
+      version = "1.2.9"
     }
   }
 }
