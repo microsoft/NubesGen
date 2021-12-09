1b463d79604ca73018f286fa7d18f6123bff80bc
Julien Dubois <julien.dubois@gmail.com>
2021-12-09 22:13:54 +0100

    Remove azuread provider dependency Thank you @felipmiguel

--- src/test/resources/nubesgen/terraform/asc-vnet-mysql-java/terraform/modules/spring-cloud/outputs.tf
+++ src/test/resources/nubesgen/terraform/asc-vnet-mysql-java/terraform/modules/spring-cloud/outputs.tf
@@ -13,11 +13,11 @@ output "application_caf_name" {
 }
 
 output "spring_cloud_service_ip" {
-  value = data.azurerm_lb.asc_internal_lb.private_ip_address
+  value       = data.azurerm_lb.asc_internal_lb.private_ip_address
   description = "Azure Spring Cloud internal load balancer IP address. Can be used to configure private DNS"
 }
 
 output "spring_cloud_service_app_fqdn" {
-  value = azurerm_spring_cloud_app.application.fqdn
+  value       = azurerm_spring_cloud_app.application.fqdn
   description = "Application FQDN"
 }
