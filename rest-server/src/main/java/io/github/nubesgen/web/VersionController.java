b7d6af84e879cb6e2fba2bf57149d1aadbc413e5
Julien Dubois <julien.dubois@gmail.com>
2022-01-10 16:59:22 +0100

    Add API to get the Version number
    Fix #180

---
+++ rest-server/src/main/java/io/github/nubesgen/web/VersionController.java
@@ -0,0 +1,21 @@
+package io.github.nubesgen.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.nubesgen.service.ConfigurationService;
+
+@RestController
+public class VersionController {
+
+    private final ConfigurationService configurationService;
+
+    public VersionController(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
+    
+    @GetMapping(value = "/api/version")
+    public String version() {
+        return "{ \"version\": \"" + this.configurationService.getVersion() + "\"}";
+    }
+}
