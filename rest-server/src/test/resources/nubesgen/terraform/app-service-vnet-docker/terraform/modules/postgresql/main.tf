7883a7aaa5936aed183fbe176b0c643867c7ac91
Julien Dubois <julien.dubois@gmail.com>
2022-01-12 17:16:08 +0100

    first test of PostgreSQL Flexible server with VNet

--- rest-server/src/test/resources/nubesgen/terraform/app-service-vnet-docker/terraform/modules/postgresql/main.tf
+++ rest-server/src/test/resources/nubesgen/terraform/app-service-vnet-docker/terraform/modules/postgresql/main.tf
@@ -27,11 +27,13 @@ resource "azurerm_postgresql_flexible_server" "database" {
   administrator_login    = var.administrator_login
   administrator_password = random_password.password.result
 
-  sku_name                         = "GP_Standard_D2ds_v4"
-  storage_mb                       = 32768
-  backup_retention_days            = 7
-  geo_redundant_backup_enabled     = false
-  version                          = "13"
+  sku_name                     = "GP_Standard_D2ds_v4"
+  storage_mb                   = 32768
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+  version                      = "13"
+  delegated_subnet_id          = var.subnet_id
+  private_dns_zone_id          = azurerm_private_dns_zone.database.id
 
   tags = {
     "environment"      = var.environment
@@ -50,4 +52,16 @@ resource "azurerm_postgresql_flexible_server_database" "database" {
   server_id           = azurerm_postgresql_flexible_server.database.id
   charset             = "utf8"
   collation           = "en_US.utf8"
+}
+
+resource "azurerm_private_dns_zone" "database" {
+  name                =  "${azurecaf_name.postgresql_database.result}.postgres.database.azure.com"
+  resource_group_name = var.resource_group
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "database" {
+  name                  = "${azurecaf_name.postgresql_database.result}-dns-link"
+  resource_group_name   = var.resource_group
+  private_dns_zone_name = azurerm_private_dns_zone.database.name
+  virtual_network_id    = var.virtual_network_id
 }
