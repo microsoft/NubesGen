cc4a6e84c13a27a4c2bbc9004142ecd2ef26b9ca
Julien Dubois <julien.dubois@gmail.com>
2022-01-13 18:30:49 +0100

    Fix Azure Spring Cloud deployement

--- rest-server/src/test/resources/nubesgen/terraform/asc-vnet-addons-java/terraform/modules/spring-cloud/main.tf
+++ rest-server/src/test/resources/nubesgen/terraform/asc-vnet-addons-java/terraform/modules/spring-cloud/main.tf
@@ -126,6 +126,11 @@ resource "azurerm_spring_cloud_java_deployment" "application_deployment" {
   }
 }
 
+resource "azurerm_spring_cloud_active_deployment" "application_deployment" {
+  spring_cloud_app_id = azurerm_spring_cloud_app.application.id
+  deployment_name     = azurerm_spring_cloud_java_deployment.application_deployment.name
+}
+
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault_access_policy" "application" {
