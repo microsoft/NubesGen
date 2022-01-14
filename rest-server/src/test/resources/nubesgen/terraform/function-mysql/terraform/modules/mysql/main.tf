af5652750e1472e65beec24896712e474e339f95
Julien Dubois <julien.dubois@gmail.com>
2022-01-14 15:31:56 +0100

    Add support for MySQL Flexible server

--- rest-server/src/test/resources/nubesgen/terraform/function-mysql/terraform/modules/mysql/main.tf
+++ rest-server/src/test/resources/nubesgen/terraform/function-mysql/terraform/modules/mysql/main.tf
@@ -19,24 +19,18 @@ resource "random_password" "password" {
   override_special = "_%@"
 }
 
-resource "azurerm_mysql_server" "database" {
+resource "azurerm_mysql_flexible_server" "database" {
   name                = azurecaf_name.mysql_server.result
   resource_group_name = var.resource_group
   location            = var.location
 
-  administrator_login          = var.administrator_login
-  administrator_login_password = random_password.password.result
+  administrator_login    = var.administrator_login
+  administrator_password = random_password.password.result
 
-  sku_name                          = "B_Gen5_1"
-  storage_mb                        = 5120
-  version                           = "5.7"
-  auto_grow_enabled                 = true
+  sku_name                          = "B_Standard_B1ms"
+  version                           = "8.0.21"
   backup_retention_days             = 7
   geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
 
   tags = {
     "environment"      = var.environment
@@ -50,10 +44,10 @@ resource "azurecaf_name" "mysql_database" {
   suffixes      = [var.environment]
 }
 
-resource "azurerm_mysql_database" "database" {
+resource "azurerm_mysql_flexible_database" "database" {
   name                = azurecaf_name.mysql_database.result
   resource_group_name = var.resource_group
-  server_name         = azurerm_mysql_server.database.name
+  server_name         = azurerm_mysql_flexible_server.database.name
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
 }
@@ -65,10 +59,10 @@ resource "azurecaf_name" "mysql_firewall_rule" {
 }
 
 # This rule is to enable the 'Allow access to Azure services' checkbox
-resource "azurerm_mysql_firewall_rule" "database" {
+resource "azurerm_mysql_flexible_server_firewall_rule" "database" {
   name                = azurecaf_name.mysql_firewall_rule.result
   resource_group_name = var.resource_group
-  server_name         = azurerm_mysql_server.database.name
+  server_name         = azurerm_mysql_flexible_server.database.name
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
 }
