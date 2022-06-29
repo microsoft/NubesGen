5c6b34eeec0f211d029f0cc1c0e249630f7f047d
Tim Yates <tim.yates@gmail.com>
2022-06-29 15:10:07 +0100

    Update Gitops action template for Micronaut plus tests

---
+++ rest-server/src/test/resources/nubesgen/terraform/app-service-micronaut-maven-gitops-postgres/terraform/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.11.0"
+    }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "1.2.16"
+    }
+  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  // If an environment is set up (dev, test, prod...), it is used in the application name
+  environment = var.environment == "" ? "dev" : var.environment
+}
+
+resource "azurecaf_name" "resource_group" {
+  name          = var.application_name
+  resource_type = "azurerm_resource_group"
+  suffixes      = [local.environment]
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = azurecaf_name.resource_group.result
+  location = var.location
+
+  tags = {
+    "terraform"        = "true"
+    "environment"      = local.environment
+    "application-name" = var.application_name
+    "nubesgen-version" = "test"
+
+    // Name of the Azure Storage Account that stores the Terraform state
+    "terraform_storage_account" = var.terraform_storage_account
+  }
+}
+
+module "application" {
+  source           = "./modules/app-service"
+  resource_group   = azurerm_resource_group.main.name
+  application_name = var.application_name
+  environment      = local.environment
+  location         = var.location
+
+  database_url      = module.database.database_url
+  database_username = module.database.database_username
+  database_password = module.database.database_password
+}
+
+module "database" {
+  source           = "./modules/postgresql"
+  resource_group   = azurerm_resource_group.main.name
+  application_name = var.application_name
+  environment      = local.environment
+  location         = var.location
+}
