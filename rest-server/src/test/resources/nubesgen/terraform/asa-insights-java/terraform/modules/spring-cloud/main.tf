08aac7e0a83b93a6efee40cdfa24ab4b7c00eee9
Julien Dubois <julien.dubois@gmail.com>
2022-11-22 16:44:36 +0100

    Replace "asc-" prefix with "asa-" for Azure Spring Apps

---
+++ rest-server/src/test/resources/nubesgen/terraform/asa-insights-java/terraform/modules/spring-cloud/main.tf
@@ -0,0 +1,48 @@
+# Azure Spring Apps is not yet supported in azurecaf_name
+locals {
+  spring_cloud_service_name = "asa-${var.application_name}-${var.environment}"
+  spring_cloud_app_name     = "app-${var.application_name}"
+}
+
+# This creates the Azure Spring Apps that the service use
+resource "azurerm_spring_cloud_service" "application" {
+  name                = local.spring_cloud_service_name
+  resource_group_name = var.resource_group
+  location            = var.location
+  sku_name            = "B0"
+
+  tags = {
+    "environment"      = var.environment
+    "application-name" = var.application_name
+  }
+  trace {
+    connection_string = var.azure_application_insights_connection_string
+  }
+}
+
+# This creates the application definition
+resource "azurerm_spring_cloud_app" "application" {
+  name                = local.spring_cloud_app_name
+  resource_group_name = var.resource_group
+  service_name        = azurerm_spring_cloud_service.application.name
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# This creates the application deployment. Terraform provider doesn't support dotnet yet
+resource "azurerm_spring_cloud_java_deployment" "application_deployment" {
+  name                = "default"
+  spring_cloud_app_id = azurerm_spring_cloud_app.application.id
+  instance_count      = 1
+  runtime_version     = "Java_17"
+
+  quota {
+    cpu    = "1"
+    memory = "1Gi"
+  }
+
+  environment_variables = {
+    "SPRING_PROFILES_ACTIVE" = "prod,azure"
+  }
+}
