e44f5c70915015b1e9e57af3f8280451359f879c
Julien Dubois <julien.dubois@gmail.com>
2023-03-10 15:09:42 +0000

    Fix templates in native build
    Fix Native build with GraalVM cannot find templates #397

--- rest-server/src/main/java/io/github/nubesgen/NubesgenApplication.java
+++ rest-server/src/main/java/io/github/nubesgen/NubesgenApplication.java
@@ -2,8 +2,10 @@ package io.github.nubesgen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(NubesgenRuntimeHints.class)
 public class NubesgenApplication {
 
     public static void main(String[] args) {
