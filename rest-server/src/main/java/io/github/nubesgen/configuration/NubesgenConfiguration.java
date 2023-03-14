41820740d0d8a2ad304dff94962d9ac5dae956fa
Julien Dubois <julien.dubois@gmail.com>
2023-03-14 18:45:21 +0100

    Upgrade to nubesgen-actions v0.11.0

--- rest-server/src/main/java/io/github/nubesgen/configuration/NubesgenConfiguration.java
+++ rest-server/src/main/java/io/github/nubesgen/configuration/NubesgenConfiguration.java
@@ -21,7 +21,7 @@ public class NubesgenConfiguration {
 
     private String applicationName;
 
-    private String compositeActionsVersion = "v0.10.0";
+    private String compositeActionsVersion = "v0.11.0";
 
     @JsonProperty("iactool")
     private IaCTool iaCTool;
