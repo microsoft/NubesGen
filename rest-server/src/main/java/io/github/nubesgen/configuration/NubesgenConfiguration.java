b64909dfca15019e9c31c037747eba0448e55059
Julien Dubois <julien.dubois@gmail.com>
2023-03-20 10:54:10 +0100

    Add application_url support to Container Apps

--- rest-server/src/main/java/io/github/nubesgen/configuration/NubesgenConfiguration.java
+++ rest-server/src/main/java/io/github/nubesgen/configuration/NubesgenConfiguration.java
@@ -21,7 +21,7 @@ public class NubesgenConfiguration {
 
     private String applicationName;
 
-    private String compositeActionsVersion = "v0.11.0";
+    private String compositeActionsVersion = "v0.12.0";
 
     @JsonProperty("iactool")
     private IaCTool iaCTool;
