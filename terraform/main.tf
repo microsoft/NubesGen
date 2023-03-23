7bcd8f0fe00d0b640904a210882437e1796e1784
Julien Dubois <julien.dubois@gmail.com>
2023-03-23 16:54:09 +0100

    Update azurerm version

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.45.0"
+      version = "3.48.0"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
